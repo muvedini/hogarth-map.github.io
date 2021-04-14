98a9169acc3e029291e2dc9f32fd1b56d2598600
Muvedini Sukumaran <64189973+muvedini@users.noreply.github.com>
2021-04-14 17:30:14 +0530

    Add files via upload

---
+++ script-map.js
@@ -0,0 +1,8 @@
+//DOM TARGETS
+const carouselPopup = document.getElementById("carousel-");
+console.log(carouselPopup);
+
+const carousel = function() {
+    carouselPopup.classList.add("visible");
+};
+console.log(carousel);
